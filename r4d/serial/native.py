daa95b81ee204fe628e0c3686a8b5cab89b377f5
David Holloway <david.holloway@windriver.com>
2020-08-16 22:57:55 -0700

    place holder

---
+++ r4d/serial/native.py
@@ -0,0 +1,18 @@
+import logging
+
+from r4d.db import SerialControl
+
+log = logging.getLogger (__name__)
+
+def register (parent):
+    log.debug ("register " + __name__)
+    parent._add_model ('NATIVE', native)
+
+class native (SerialControl):
+    __mapper_args__ = {'polymorphic_identity': 'NATIVE'}
+
+    def num_ports (self):
+        return 8
+
+    def get_udpport (self, port):
+        return (7000 + port)
